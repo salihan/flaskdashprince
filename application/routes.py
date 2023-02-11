ac3412ec7bc519810d74527051fabd8f923cdf46
salihan <salihan@yahoo.com>
2023-02-11 23:30:22 +0800

    UI improvement - try to make it visible without needing to scroll

--- application/routes.py
+++ application/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, url_for
 import pandas as pd
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 import json
 from sqlalchemy import create_engine
 
@@ -47,10 +48,10 @@ df_tlel = pd.read_sql(query_tlel, engine)
 # fig1.update_xaxes(showticklabels=False)
 # fig1.update_layout(margin=dict(l=0, r=10, t=30, b=0))
 
-fig1 = px.pie(df_tlel, values='# of Students', names='Course Code', title='Total Learners in Each Lesson', height=300)
+fig1 = px.pie(df_tlel, values='# of Students', names='Course Code', title='Total Learners in Each Lesson', height=200)
 fig1.update_traces(textposition='inside', showlegend=False)
 fig1.update_xaxes(showticklabels=False)
-fig1.update_layout(margin=dict(l=0, r=10, t=30, b=0))
+fig1.update_layout(margin=dict(l=0, r=10, t=30, b=0), font=dict(size=10,color="RebeccaPurple"))
 
 graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
 
@@ -63,9 +64,9 @@ ORDER BY timecreated DESC;
 """
 df_usersmontly = pd.read_sql(query_usersmontly, engine)
 fig2 = px.scatter(df_usersmontly, x='Month', y='New User', title='New Users Monthly',
-                  height=300, color='New User')
+                  height=200, color='New User', color_continuous_scale='Bluered_r')
 fig2.update_xaxes(showticklabels=False)
-fig2.update_layout(margin=dict(l=0, r=10, t=30, b=0))
+fig2.update_layout(margin=dict(l=0, r=10, t=30, b=0), font=dict(size=10,color="RebeccaPurple"))
 graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
 
 #6) monthly access frequency
@@ -79,19 +80,75 @@ ORDER BY timecreated DESC;
 df_accessfreq = pd.read_sql(query_accessfreq, engine)
 df_accessfreq['Month'] = pd.to_datetime(df_accessfreq['Month'], format='%Y-%m')
 
-fig3 = px.scatter(df_accessfreq, x='Month', y='Access Frequency', title='Monthly Access Frequency', height=300, trendline="ols")
+fig3 = px.scatter(df_accessfreq, x='Month', y='Access Frequency', title='Monthly Access Frequency', height=200, trendline="ols")
 fig3.update_xaxes(showticklabels=False)
-fig3.update_layout(margin=dict(l=0, r=10, t=30, b=0))
+fig3.update_layout(margin=dict(l=0, r=10, t=30, b=0), font=dict(size=10,color="RebeccaPurple"))
 graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
 
 #7) users' country
 query_usercountry = "SELECT country, COUNT(*) AS count FROM mdl_user GROUP BY country"
 df_usercountry = pd.read_sql(query_usercountry, engine)
+
+# fig_map = go.Figure(data=go.Choropleth(
+#     locations=df_usercountry['country'],
+#     z=df_usercountry['count'],
+#     # text=df_usercountry['country'],
+#     colorscale='Inferno',
+#     autocolorscale=False,
+#     reversescale=True,
+#     marker_line_color='darkgray',
+#     marker_line_width=0.5,
+#     # colorbar_title='Years',
+# ))
+fig_map = go.Figure(data=go.Choropleth(
+    locations=df_usercountry['country'],
+    z=df_usercountry['count'],
+    colorscale='Inferno',
+    autocolorscale=False,
+    reversescale=True,
+    marker_line_color='darkgray',
+    marker_line_width=0.5,
+    colorbar=dict(
+        x=0,
+        y=0.5,
+        len=0.5,
+        orientation='h'
+    )
+))
+
+# fig_map.update_coloraxes(colorbar_orientation="h")
+fig_map.update_layout(
+    # width=1000,
+    # height=620,
+    geo=dict(
+        showframe=False,
+        showcoastlines=False,
+        projection_type='equirectangular'
+    ),
+    title={
+        'text': 'Map Country of Users',
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top',
+    }
+    # title_font_color='#525252',
+    # title_font_size=10,
+    # font=dict(
+    #     family='Heebo',
+    #     size=18,
+    #     color='#525252'
+    # )
+)
+
 # Create a plotly choropleth map of users' countries
-fig_map = px.choropleth(df_usercountry, locations="country", color="count",
-                        color_discrete_sequence=px.colors.sequential.Plasma,
-                        title='Map Country of Users')
-fig_map.update_layout(margin=dict(l=0, r=10, t=30, b=0))
+# fig_map = px.choropleth(df_usercountry, locations="country", color="count",
+#                         color_discrete_sequence=px.colors.sequential.Plasma,
+#                         title='Map Country of Users')
+
+fig_map.update_layout(margin=dict(l=0, r=0, t=0, b=0, pad=0), font=dict(size=10,color="RebeccaPurple"))
+# fig_map.update(layout_coloraxis_showscale=False)
+# fig_map.update_traces(colorbar_orientation='horizontal', selector=dict(type='heatmap'))
 graph4JSON = json.dumps(fig_map, cls=plotly.utils.PlotlyJSONEncoder)
 
 @app.route("/")
